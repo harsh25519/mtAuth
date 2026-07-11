a032e15ad8a2f6f70f8ff209907ea3029a1df5c8
HarshBhardwaj <harshbdj25@gmail.com>
2026-07-11 21:14:33 +0530

    Removed admin seeder

--- src/main/java/bdj/hkb/auth_service/config/SecurityConfig.java
+++ src/main/java/bdj/hkb/auth_service/config/SecurityConfig.java
@@ -99,13 +99,16 @@ public class SecurityConfig {
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
                 "http://127.0.0.1:8081",
-                "http://localhost:8081",
-                "http://127.0.0.1:8080",
-                "http://localhost:8080"
+                "http://localhost:8081"
         ));
 
-        config.setAllowedMethods(List.of("PUT", "OPTIONS"));
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowedMethods(List.of("GET",
+                "POST",
+                "PUT",
+                "PATCH",
+                "DELETE",
+                "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         config.setExposedHeaders(List.of("Authorization"));
         config.setMaxAge(3600L);
